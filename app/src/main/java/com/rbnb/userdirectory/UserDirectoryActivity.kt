4812ceb862e6573f3c899fee2a5df239cf09fd30
Renner Bernabe <rennerbrian.bernabe@gmail.com>
2021-04-24 12:26:47 +0800

    Initial commit

---
+++ app/src/main/java/com/rbnb/userdirectory/UserDirectoryActivity.kt
@@ -0,0 +1,11 @@
+package com.rbnb.userdirectory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserDirectoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_directory)
+    }
+}
---
+++ app/src/main/java/com/rbnb/userdirectory/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.rbnb.userdirectory.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
