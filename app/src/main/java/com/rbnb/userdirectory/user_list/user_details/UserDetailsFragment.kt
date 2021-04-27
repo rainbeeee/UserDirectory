f178af3b53e3c64c87bc823e5aabf3cf0cbb75b7
Renner Bernabe <rennerbrian.bernabe@gmail.com>
2021-04-28 00:07:38 +0800

    Display location on map using Maps SDK

--- app/src/main/java/com/rbnb/userdirectory/user_list/user_details/UserDetailsFragment.kt
+++ app/src/main/java/com/rbnb/userdirectory/user_list/user_details/UserDetailsFragment.kt
@@ -5,21 +5,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.rbnb.userdirectory.R
 import com.rbnb.userdirectory.databinding.FragmentUserDetailsBinding
 
 class UserDetailsFragment : Fragment() {
 
+    private val viewModel: UserDetailsViewModel by viewModels {
+        UserDetailsViewModelFactory(safeArgs.user)
+    }
+
     private val safeArgs: UserDetailsFragmentArgs by navArgs()
+    private val mapCallback = OnMapReadyCallback { googleMap ->
+        val addressLatLng = LatLng(viewModel.lat, viewModel.long)
+        googleMap.addMarker(MarkerOptions().position(addressLatLng).title(viewModel.city))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(addressLatLng))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         val binding = FragmentUserDetailsBinding.inflate(inflater, container, false)
-        binding.user = safeArgs.user
+        binding.user = viewModel.user
         binding.lifecycleOwner = viewLifecycleOwner
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.mapUserDetails) as SupportMapFragment?
+        mapFragment?.getMapAsync(mapCallback)
     }
 }
--- app/src/main/java/com/rbnb/userdirectory/user_list/user_details/UserDetailsViewModel.kt
+++ app/src/main/java/com/rbnb/userdirectory/user_list/user_details/UserDetailsViewModel.kt
@@ -1,7 +1,28 @@
 package com.rbnb.userdirectory.user_list.user_details
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.rbnb.userdirectory.user_list.User
 
-class UserDetailsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class UserDetailsViewModel(val user: User) : ViewModel() {
+
+    val lat: Double
+        get() = user.address.geo.lat.toDouble()
+
+    val long: Double
+        get() = user.address.geo.lng.toDouble()
+
+    val city: String
+        get() = user.address.city
+}
+
+class UserDetailsViewModelFactory(private val user: User?) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(UserDetailsViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return user?.let { UserDetailsViewModel(it) } as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
